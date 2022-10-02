3ebec204e702167ebc1fe37eac9eb1de9111548d
androborn <andrii.titov@gmail.com>
2022-10-02 22:26:55 +0300

    [chore] simplify file structure
    *remove src\components\Feedback\Feedback.jsx
    *move Feedback component functionality to src\App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,69 @@
-import { Feedback } from "./components";
-import { Wrapper } from "./App.styled";
+// import { Feedback } from "./components";
+import { Component } from 'react';
+import {
+  FeedbackOptions,
+  Statistics,
+  Section,
+  Notification,
+} from './components';
 
-export default function App() {
-  return (
-    <Wrapper className="App">
-      <Feedback />
-    </Wrapper>
-  );
+import { Wrapper } from './App.styled';
+
+export default class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  updateState = e => {
+    const targetButtonContent = e.target.textContent;
+
+    this.setState(() => ({
+      [targetButtonContent]: this.state[targetButtonContent] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => Object.values(this.state).reduce((a, b) => a + b);
+
+  countPositiveFeedbackPercentage = () => {
+    const positiveFeedbackQuantity = this.state.good;
+    const totalFeedbackQuantity = this.countTotalFeedback();
+
+    return Math.round((positiveFeedbackQuantity / totalFeedbackQuantity) * 100);
+  };
+
+  render() {
+    const {
+      state,
+      updateState,
+      countTotalFeedback,
+      countPositiveFeedbackPercentage,
+    } = this;
+
+    return (
+      <Wrapper>
+        <Section title="Please leave a feedback">
+          <FeedbackOptions
+            options={Object.keys(state)}
+            feedbackHandler={updateState}
+            type={'button'}
+          ></FeedbackOptions>
+        </Section>
+        <Section title="Statistics">
+          {countTotalFeedback() ? (
+            <Statistics
+              good={state.good}
+              neutral={state.neutral}
+              bad={state.bad}
+              totalFeedbackCount={countTotalFeedback()}
+              positiveFeedbackPercentage={countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback yet"></Notification>
+          )}
+        </Section>
+      </Wrapper>
+    );
+  }
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
-export { Feedback } from "./Feedback/Feedback";
-export { Statistics } from "./Statistics/Statistics";
-export { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
-export { Section } from "./Section/Section";
-export { Notification } from "./Notification/Notification";
+export { Statistics } from './Statistics/Statistics';
+export { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+export { Section } from './Section/Section';
+export { Notification } from './Notification/Notification';
